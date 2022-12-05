c5fa23d96cafe37e4ff425047897422fef5a8747
pxnandi <88222082+pxnandi@users.noreply.github.com>
2022-12-05 14:24:36 +0700

    Add files via upload

---
+++ names.py
@@ -0,0 +1,25 @@
+from random import *
+import random
+
+consonants = ['Б', 'В', 'Г', 'Д', 'Ж', 'З', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ']
+mainOrder = []
+k = 0
+endName = ''
+
+inputName = (input('Введите имя: '))
+Name = list(inputName.upper())
+nameLen = len(Name)
+
+for i in Name:
+    for c in consonants:
+        if i == c:
+            mainOrder.append(k)
+    k += 1    
+
+for i in range(randint(1, len(mainOrder))):
+    Name[mainOrder[randint(0, (len(mainOrder))-1)]] = random.choice(consonants)
+
+for i in range(nameLen):
+    endName += Name[i]
+
+print(endName)
