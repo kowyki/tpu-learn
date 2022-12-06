2ac8e7e84d44bf2cb6ffd6eb35df15dd34d9bb7d
pxnandi <soplifan.rifamas@gmail.com>
2022-12-06 23:23:40 +0700

    update

--- names.py
+++ names.py
@@ -2,24 +2,39 @@ from random import *
 import random
 
 consonants = ['Б', 'В', 'Г', 'Д', 'Ж', 'З', 'Й', 'К', 'Л', 'М', 'Н', 'П', 'Р', 'С', 'Т', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ']
-mainOrder = []
-k = 0
-endName = ''
 
-inputName = (input('Введите имя: '))
-Name = list(inputName.upper())
-nameLen = len(Name)
+def mainP():
+    mainOrder = []
+    k = 0
+    endName = ''
 
-for i in Name:
-    for c in consonants:
-        if i == c:
-            mainOrder.append(k)
-    k += 1    
+    inputName = (input('Введите имя: '))
+    Name = list(inputName.upper())
+    nameLen = len(Name)
 
-for i in range(randint(1, len(mainOrder))):
-    Name[mainOrder[randint(0, (len(mainOrder))-1)]] = random.choice(consonants)
+    for i in Name:
+        for c in consonants:
+            if i == c:
+                mainOrder.append(k)
+        k += 1
 
-for i in range(nameLen):
-    endName += Name[i]
+    if mainOrder == []:
+        print('Изменение невозможно')
+        input()
+        quit()
 
-print(endName)
+    for i in range(randint(1, len(mainOrder))):
+        Name[mainOrder[randint(0, (len(mainOrder))-1)]] = random.choice(consonants)
+
+    for i in range(nameLen):
+        endName += Name[i]
+
+    print(endName)
+    input()
+    
+while True:
+    ans = int(input('1. Изменить имя \n2. Выйти \n'))
+    if ans == 1:
+            mainP()
+    if ans == 2:
+        quit()
