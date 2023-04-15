ef5dc34fef12ee94103cd305611fcb317ae45fcf
pxnandi <88222082+pxnandi@users.noreply.github.com>
2023-04-15 10:09:49 +0700

    Delete xyzw.py

--- xyzw.py
+++
@@ -1,8 +0,0 @@
-print("x y z w")
-
-for x in 0, 1:
-    for y in 0, 1:
-        for z in 0, 1:
-            for w in 0, 1:
-                if (not (y<=x) or (z<=w) or not (z)) == 0:
-                    print(x, y, z , w)
