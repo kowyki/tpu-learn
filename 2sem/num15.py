a2b6a2702306e5071d26a7446f4a91292e0fbe18
pxnandi <88222082+pxnandi@users.noreply.github.com>
2023-04-15 10:03:50 +0700

    Delete 2sem directory

--- 2sem/num15.py
+++
@@ -1,7 +0,0 @@
-# Demo-23
-def d23():
-    for A in range(1, 100000):
-        if all(((x % 2 == 0) <= (x % 3 != 0)) or (x + A >= 100) for x in range(1, 100)):
-            print(A)
-            break
-
